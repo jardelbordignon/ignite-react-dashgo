f1e050f507e2e46b72997cdb4ad7d01557e3e345
Jardel <jardel1101@gmail.com>
2021-04-30 12:00:45 -0300

    Componente: Sidebar

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,10 +1,16 @@
+import { Flex } from '@chakra-ui/react'
+
 import { Header } from '../components/Header';
+import { Sidebar } from '../components/Sidebar';
 
 export default function DashBoard() {
   return (
-    <>
+    <Flex direction='column' h='100vh'>
       <Header />
-      <h1>DashBoard</h1>
-    </>
+      
+      <Flex w='100%' my='6' mx='auto' maxWidth={1480} px='6'>
+        <Sidebar />
+      </Flex>
+    </Flex>
   )
 }
