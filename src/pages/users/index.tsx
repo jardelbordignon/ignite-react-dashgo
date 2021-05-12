a5125a8bf8e7c1086700892c0c8022430f4ba05d
Jardel <jardel1101@gmail.com>
2021-05-12 13:22:29 -0300

    Sinalizando refetch dos dados

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -23,7 +23,7 @@ import { Pagination } from '../../components/Pagination'
 import { Sidebar } from '../../components/Sidebar'
 
 export default function UserList() {
-  const { data, isLoading, error } = useQuery('users', async () => {
+  const { data, isLoading, error, isFetching } = useQuery('users', async () => {
     const response = await fetch('http://localhost:3000/api/users')
     const data = await response.json()
 
@@ -57,7 +57,12 @@ export default function UserList() {
 
         <Box borderRadius={8} bg='gray.800' p={['4', '6', '8']} w='100%'>
           <Flex mb='8' justify='space-between' align='center'>
-            <Heading size='lg' fontWeight='normal'>Usuários</Heading>
+            <Heading size='lg' fontWeight='normal'>
+              Usuários
+              { !isLoading && isFetching && 
+                <Spinner size='sm' color='gray.500' ml='4' />
+              } 
+            </Heading>
             <Link href='/users/create' passHref>
               <Button
                 as='a'
