c0b0e01b5408dab95d4bac66ab84a24e824b55c9
Jardel <jardel1101@gmail.com>
2021-05-12 14:47:12 -0300

    Criando hook de listagem

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -16,32 +16,14 @@ import {
   useBreakpointValue} from '@chakra-ui/react'
 import { RiAddLine, RiPencilLine } from 'react-icons/ri'
 import Link from 'next/link'
-import { useQuery } from 'react-query'
 
 import { Header } from '../../components/Header'
 import { Pagination } from '../../components/Pagination'
 import { Sidebar } from '../../components/Sidebar'
-import { api } from '../../services/api'
+import { useUsers } from '../../services/hooks/useUsers'
 
 export default function UserList() {
-  const { data, isLoading, error, isFetching } = useQuery('users', async () => {
-    const { data } = await api.get('/users')
-
-    const users = data.users.map(user => ({
-      id: user.id,
-      name: user.name,
-      email: user.email,
-      createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-        day: '2-digit',
-        month: 'long',
-        year: 'numeric'
-      })
-    }))
-
-    return users
-  }, {
-    staleTime: 1000 * 3
-  })
+  const { data, isLoading, error, isFetching } = useUsers()
 
   const isWideVersion = useBreakpointValue({
     base: false,
